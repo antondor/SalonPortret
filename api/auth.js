fbffe4f227dc123e9650c7b4429328aa9ab7eeea
Maiev <antdor24@gmail.com>
2026-06-06 17:43:44 +0300

    netlify

--- api/auth.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const client_id = process.env.GITHUB_CLIENT_ID;
-  
-  // Жестко указываем домен, чтобы браузер не сбрасывал связь между окнами
-  const redirect_uri = `https://salonportret.vercel.app/api/callback`;
-  
-  const url = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=repo,user`;
-  res.redirect(url);
-}
